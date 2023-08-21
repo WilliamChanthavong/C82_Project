83cd5a482ede836ce775cc1f9775fcbc6de4e210
WilliamChanthavong <121891997+WilliamChanthavong@users.noreply.github.com>
2023-08-20 20:18:59 -0700

    Update main.js

--- main.js
+++ main.js
@@ -16,6 +16,24 @@ function my_mousedown(e)
     width_of_line = document.getElementById("width_of_line").value;
 
     mouseEvent = "mouseDown";
+
+    color = document.getElementById("color").value;
+    console.log(color);
+
+    mouse_x = e.clientX - canvas.offsetLeft;
+    mouse_y = e.clientY - canvas.offsetTop;
+
+    console.log("X = " + mouse_x + " , Y = " + mouse_y)
+    circle(mouse_x , mouse_y)
+}
+
+function circle(mouse_x , mouse_y)
+{
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 2;
+    ctx.arc(mouse_x, mouse_y, width_of_line, 0, 2*Math.PI);
+    ctx.stroke();
 }
 
 canvas.addEventListener("mouseleave",my_mouseleave);
@@ -39,19 +57,27 @@ function my_mousemove(e)
     current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
     current_position_of_mouse_y = e.clientY - canvas.offsetTop;
 
+    color = document.getElementById("color").value;
+    console.log(color);
+
+    mouse_x = e.clientX - canvas.offsetLeft;
+    mouse_y = e.clientY - canvas.offsetTop;
+
     if (mouseEvent == "mouseDown") {
-        ctx.beginPath();
-        ctx.strokeStyle = color;
-        ctx.lineWidth = width_of_line;
+    //     ctx.beginPath();
+    //     ctx.strokeStyle = color;
+    //     ctx.lineWidth = width_of_line;
 
-        console.log("Last position of x and y coordinates = ");
-        console.log("x = " + last_position_of_x + "y = " + last_position_of_y);
-        ctx.moveTo(last_position_of_x,last_position_of_y);
+    //     console.log("Last position of x and y coordinates = ");
+    //     console.log("x = " + last_position_of_x + "y = " + last_position_of_y);
+    //     ctx.moveTo(last_position_of_x,last_position_of_y);
 
-        console.log("Current position of x and y coordinates = ");
-        console.log("x = " + current_position_of_mouse_x + "y = " + current_position_of_mouse_y);
-        ctx.lineTo(current_position_of_mouse_x, current_position_of_mouse_y);
-        ctx.stroke()
+    //     console.log("Current position of x and y coordinates = ");
+    //     console.log("x = " + current_position_of_mouse_x + "y = " + current_position_of_mouse_y);
+    //     ctx.lineTo(current_position_of_mouse_x, current_position_of_mouse_y);
+    //     ctx.stroke()
+    console.log("X = " + mouse_x + " , Y = " + mouse_y)
+    circle(mouse_x , mouse_y)
     }
 
     last_position_of_x = current_position_of_mouse_x;
